ae5d5d17c750be99d2d887dd5a82bef312a3fa35
Elyas <Elyas.shamal2@gmail.com>
2022-10-29 16:57:45 -0400

    Rocketman webpage

---
+++ script.js
@@ -0,0 +1,7 @@
+
+const toggleButton = document.getElementById("toggle-Button");
+const navList = document.getElementById("nav-list");
+
+toggleButton.addEventListener('click', () => {
+    navList.classList.toggle('active');
+})
